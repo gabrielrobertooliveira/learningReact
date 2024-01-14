0d0f8893568a19485e86c02f5a49a8a3fca068e7
gabrielrobertooliveira <gabrielroberto1605@gmail.com>
2024-01-14 14:18:00 -0300

    Projeto React iniciado

---
+++ projeto-react/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './components/HelloWorld';
+import Frase from './components/Frase';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld/>
+      <Frase/>
+    </div>
+  );
+}
+
+export default App;
---
+++ projeto-react/src/components/Frase.js
@@ -0,0 +1,9 @@
+function Frase(){
+    return(
+        <div>
+            <p>Esta é uma frase qualquer</p>
+        </div>    
+    )
+    
+}
+export default Frase
