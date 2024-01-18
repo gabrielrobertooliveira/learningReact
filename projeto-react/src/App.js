ee6ea5259578c03d8fabd26ddfbe474cdf767267
gabrielrobertooliveira <gabrielroberto1605@gmail.com>
2024-01-18 14:00:17 -0300

    Utilizando props de forma simples

--- projeto-react/src/App.js
+++ projeto-react/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import HelloWorld from './components/HelloWorld';
 import Frase from './components/Frase';
+import SayMyName from './components/SayMyName';
 
 function App() {
   return (
     <div className="App">
       <HelloWorld/>
       <Frase/>
+      <SayMyName nome="Gabriel Roberto"/>
     </div>
   );
 }
---
+++ projeto-react/src/components/SayMyName.js
@@ -0,0 +1,11 @@
+function SayMyName(props){
+    return(
+        <div>
+            <h1>
+                Bem-vindo {props.nome}
+            </h1>
+        </div>
+    )
+}
+
+export default SayMyName
